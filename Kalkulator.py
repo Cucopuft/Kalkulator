6be22d3be6d5165ace080233a7c3450b1431271a
Mae <aratnairamaya@gmail.com>
2022-09-18 15:00:34 +0700

    Kalkulator

---
+++ Kalkulator.py
@@ -0,0 +1,17 @@
+# Membuat Kalkulator Sederhana
+
+# Fungsi ini digunakan untuk penambahan dua angka
+def add(x, y):
+    return x + y
+
+# Fungsi ini digunakan untuk mengurangi dua angka
+def subtract(x, y):
+    return x - y
+
+# Fungsi ini mengalikan dua angka
+def multiply(x, y):
+    return x * y
+
+# Fungsi ini digunakan untuk pembagian 
+def divide(x, y):
+    return x / y
